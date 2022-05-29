0b24ed7132db9fc3f830df06b8fc5dd091f2e4ad
Semenov Petr <under_karter49@icloud.com>
2022-05-30 02:20:17 +0300

    Add timer.
    Add a timer and add func changing buttons.

--- Pomodoro UIKit/ViewController.swift
+++ Pomodoro UIKit/ViewController.swift
@@ -21,6 +21,12 @@ class ViewController: UIViewController {
     var startButton = UIButton()
     var stopButton = UIButton()
     
+    var timer = Timer()
+    var isTimerStarted = false
+    
+    var seconds = 10
+    let timeForWork = 10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
@@ -47,8 +53,8 @@ class ViewController: UIViewController {
         
         stopButton = UIButton(frame: CGRect(x: 220, y: view.frame.size.height - 90, width: view.frame.size.width - 250, height: 50))
         view.addSubview(stopButton)
-        stopButton.isEnabled = false
-        stopButton.alpha = 0.5
+//        stopButton.isEnabled = true
+//        stopButton.alpha = 1.0
         stopButton.setTitle("Stop", for: .normal)
         stopButton.setTitleColor(UIColor.white, for: .normal)
         stopButton.backgroundColor = .systemTeal
@@ -56,12 +62,112 @@ class ViewController: UIViewController {
         stopButton.addTarget(self, action: #selector(stopButtonAction), for: .touchUpInside)
     }
     
+    // MARK: - Button action
+    
     @objc func startButtonAction() {
+        startButton.isEnabled = true
+        startButton.alpha = 1.0
         
+        if !isTimerStarted {
+            
+            startTimer()
+            
+            isTimerStarted = true
+            
+            
+            startButton.setTitle("Pause", for: .normal)
+            startButton.setTitleColor(UIColor.white, for: .normal)
+            startButton.backgroundColor = UIColor.red
+            
+            stopButton.isEnabled = true
+            stopButton.alpha = 1
+            
+        } else {
+            timer.invalidate()
+            
+            isTimerStarted = false
+            
+            startButton.setTitle("Start", for: .normal)
+            startButton.setTitleColor(UIColor.white, for: .normal)
+            startButton.backgroundColor = UIColor.systemIndigo
+            
+        }
     }
     
     @objc func stopButtonAction() {
+        timer.invalidate()
         
+        stopButton.isEnabled = false
+        stopButton.alpha = 0.5
+        startButton.setTitle("Start", for: .normal)
+        startButton.setTitleColor(UIColor.white, for: .normal)
+        startButton.backgroundColor = UIColor.systemIndigo
+        
+        seconds = timeForWork
+        timeLabel.text = "25 : 00"
+        
+        isTimerStarted = false
+    }
+    
+    //MARK: - Timer func
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1,
+                                     target: self,
+                                     selector: #selector(ViewController.timerAction),
+                                     userInfo: nil,
+                                     repeats: true)
+    }
+    
+    @objc func timerAction(){
+        
+        seconds -= 1
+        
+        let time = secondsToMinuteSecond(time: seconds)
+        let timeString = makeTimeToString(minuties: time.0, seconds: time.1)
+        
+        timeLabel.text = timeString
+        timeLabel.sizeToFit()
+    
+        if seconds == 0 {
+            timer.invalidate()
+            
+            startButton.setTitle("Start", for: .normal)
+            startButton.setTitleColor(UIColor.white, for: .normal)
+            startButton.backgroundColor = UIColor.systemIndigo
+            
+            seconds = timeForWork
+            
+            stopButtonAction()
+            startButtonAction()
+        }
+    }
+    
+    //MARK: - label func
+    func secondsToMinuteSecond(time: Int) -> (Int, Int) {
+        let minuties = Int((Double(time)) / 60)
+        let seconds = (time % 60)
+
+        return (minuties, seconds)
+    }
+    
+    func makeTimeToString(minuties: Int, seconds: Int) -> String {
+        
+        var timeString = ""
+        if minuties >= 10 {
+        timeString += String(minuties)
+        } else {
+        timeString += String(format: "0%2i", minuties)
+        }
+        
+        timeString += " : "
+        
+        if seconds >= 10 {
+        timeString += String(seconds)
+        } else {
+        timeString += String(format: "0%2i", seconds)
+        }
+        
+        return timeString
     }
 }
 
